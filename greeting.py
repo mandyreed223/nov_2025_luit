0944b04ce4464b30098378f566ba224e86cdc871
mandyreed223 <mandy.reed223@gmail.com>
2025-11-20 21:12:26 -0500

    greeting

---
+++ greeting.py
@@ -0,0 +1,8 @@
+# Store the word "Hello" in a variable
+hello = "Hello"                          
+# Ask the user for their name and store it
+name = input("What's your name?")        
+# Combine "Hello" + space + the user's name
+greeting = hello + " " + name            
+# Print the final greeting
+print(greeting)                          
